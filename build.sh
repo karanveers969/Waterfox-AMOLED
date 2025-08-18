1283ae57ad6e83e3c32f84ca0fe8ed871c0faf2d
Karanveer Singh <135799357+karanveers969@users.noreply.github.com>
2025-08-18 23:53:26 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ wget -q https://raw.githubusercontent.com/iBotPeaches/Apktool/master/scripts/lin
 chmod +x apktool*
 
 echo "[*] Decompiling APK..."
-rm -rf patched patched_signed.apk
+rm -rf patched patched-aligned.apk
 ./apktool d latest.apk -o patched
 rm -rf patched/META-INF
 
@@ -29,11 +29,11 @@ sed -i 's/<color name="fx_mobile_layer_color_1">.*/<color name="fx_mobile_layer_
 sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' patched/res/values-night/colors.xml
 sed -i 's/<color name="fx_mobile_action_color_secondary">.*/<color name="fx_mobile_action_color_secondary">#ff25242b<\/color>/g' patched/res/values-night/colors.xml
 sed -i 's/<color name="button_material_dark">.*/<color name="button_material_dark">#ff25242b<\/color>/g' patched/res/values/colors.xml
-sed -i 's/ff1c1b22/ff000000/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff2b2a33/ff000000/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff42414d/ff15141a/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff52525e/ff25232e/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff5b5b66/ff2d2b38/g' patched/smali*/mozilla/components/ui/colors/PhotonColors.smali
+find patched/ -type f -name "PhotonColors.smali" -exec sed -i 's/ff1c1b22/ff000000/g' {} +
+find patched/ -type f -name "PhotonColors.smali" -exec sed -i 's/ff2b2a33/ff000000/g' {} +
+find patched/ -type f -name "PhotonColors.smali" -exec sed -i 's/ff42414d/ff15141a/g' {} +
+find patched/ -type f -name "PhotonColors.smali" -exec sed -i 's/ff52525e/ff25232e/g' {} +
+find patched/ -type f -name "PhotonColors.smali" -exec sed -i 's/ff5b5b66/ff2d2b38/g' {} +
 sed -i 's/1c1b22/000000/g' patched/assets/extensions/readerview/readerview.css
 sed -i 's/eeeeee/e3e3e3/g' patched/assets/extensions/readerview/readerview.css
 sed -i 's/mipmap\/ic_launcher_round/drawable\/ic_launcher_foreground/g' patched/res/drawable-v23/splash_screen.xml
@@ -43,4 +43,5 @@ echo "[*] Rebuilding APK..."
 ./apktool b patched -o patched.apk --use-aapt2
 
 echo "[*] Aligning APK..."
-zipalign -f 4 patched.apk patched_signed.apk
+# CHANGED: Output a clearly named, unsigned but aligned APK
+zipalign -f 4 patched.apk patched-aligned.apk
